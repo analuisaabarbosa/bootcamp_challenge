61968fa3026eebe324b614d6b3a96c77631cd261
analuisa <analuisaaugustob@gmail.com>
2024-07-04 17:25:47 -0300

    :sparkles:feat: Implementando as classes Curso e mentoria do pacote dominio.

---
+++ src/main/dominio/Curso.java
@@ -0,0 +1,7 @@
+package main.dominio;
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria; 
+}
---
+++ src/main/dominio/Mentoria.java
@@ -0,0 +1,9 @@
+package main.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+}
