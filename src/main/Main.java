bd4f71798501ff4447b1fe4b5b6e87b8e44f8ed5
analuisa <analuisaaugustob@gmail.com>
2024-07-08 15:59:27 -0300

    :sparkles:feat: Interligando as classes de dominio na classe Main.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,29 +2,52 @@ package main;
 
 import java.time.LocalDate;
 
+import main.dominio.Bootcamp;
 import main.dominio.Curso;
+import main.dominio.Dev;
 import main.dominio.Mentoria;
 
 public class Main {
     public static void main(String[] args) {
-        Curso curso = new Curso();
-        curso.setTitulo("Curso Java");
-        curso.setDescricao("Curso introdutório da linguagem de programação Java.");
-        curso.setCargaHoraria(8);
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("Curso Java");
+        cursoJava.setDescricao("Curso introdutório da linguagem de programação Java.");
+        cursoJava.setCargaHoraria(8);
 
-        Curso curso2 = new Curso();
-        curso2.setTitulo("Curso Python");
-        curso2.setDescricao("Curso introdutório da linguagem de programação Python.");
-        curso2.setCargaHoraria(10);
+        Curso cursoPython = new Curso();
+        cursoPython.setTitulo("Curso Python");
+        cursoPython.setDescricao("Curso introdutório da linguagem de programação Python.");
+        cursoPython.setCargaHoraria(10);
 
-        Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("Mentoria Java");
-        mentoria.setDescricao("Mentoria a introdução de Java");
-        mentoria.setData(LocalDate.now());
+        Mentoria mentoriaJava = new Mentoria();
+        mentoriaJava.setTitulo("Mentoria Java");
+        mentoriaJava.setDescricao("Mentoria a introdução de Java");
+        mentoriaJava.setData(LocalDate.now());
 
-        System.out.println(curso);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Bootcamp de introdução para desenvolvimento com Java");
+        bootcamp.getConteudos().add(cursoJava);
+        bootcamp.getConteudos().add(cursoPython);
+        bootcamp.getConteudos().add(mentoriaJava);
 
+        Dev devAna = new Dev();
+        devAna.setNome("Ana Luisa");
+        devAna.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos de Ana: " + devAna.getConteudosInscritos());
+        devAna.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos de Ana: " + devAna.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos de Ana: " + devAna.getConteudosConcluidos());
+        System.out.println("XP de Ana: " + devAna.calcularTotalXP());
+
+        System.out.println("-----");
+
+        Dev devMari = new Dev();
+        devMari.setNome("Mariane");
+        devMari.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos de Mari: " + devMari.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos de Mari: " + devMari.getConteudosConcluidos());
+        System.out.println("XP de Mari: " + devMari.calcularTotalXP());
     }
 }
